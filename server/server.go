b61ce1e04395b102d0bd727b5d6dc2c2c2c5ab61
Anant <Anantgarg454@gmail.com>
2026-01-24 01:52:27 +0530

    refactor CORS configuration and update server port handling

--- server/server.go
+++ server/server.go
@@ -1,22 +1,24 @@
 package server
 
 import (
-	"ai-powered-health-bot/config"
 	"ai-powered-health-bot/router"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Start() {
-	config := config.GetConfig()
 	r := gin.Default()
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = config.GetStringSlice("cors_allow_origin")
-	// corsConfig.AllowHeaders = []string{"Authorization", "content-type"}
-	corsConfig.AllowHeaders = []string{"*"}
-	corsConfig.AllowMethods = []string{"DELETE", "PUT", "GET", "POST"}
-	r.Use(cors.New(corsConfig))
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"*"},
+	}))
 	router.Initialize(r)
-	r.Run(config.GetString("server.host") + ":" + config.GetString("server.port"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
 }
